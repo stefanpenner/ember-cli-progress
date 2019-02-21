7cf427defba51470ee250d06a250c3569221db09
Stefan Penner <stefan.penner@gmail.com>
2019-02-21 01:13:10 -0800

    add TODO's

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const heimdalljs = require('heimdalljs');
 const chalk = require('chalk');
 const ora = require('ora');
 
+// TODO: unit test
 function progress() {
   let current = heimdalljs.current;
   let stack = [current.id.name];
@@ -16,6 +17,7 @@ function progress() {
 module.exports = {
   name: require('./package').name,
   preBuild() {
+    // TODO: extract and test
     if (this.ui.ci) { return; }
     this.ui.stopProgress()
     this.spinner =  ora(chalk.green('building... ')).start();
@@ -26,7 +28,13 @@ module.exports = {
     }, 1000/60);
   },
   postBuild() {
+    // TODO: extract and test
     if (this.ui.ci) { return; }
+    this.spinner.stop();
+    clearInterval(this.interval);
+  },
+  buildError() {
+    // TODO: extract and test
     this.spinner.stop();
     clearInterval(this.interval);
   }
